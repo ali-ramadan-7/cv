cdd74702e6c8442cbda86263b32de1ec049885f9
alirama <aliramadan453@gmail.com>
2023-10-03 11:04:16 +0300

    welcom-page

---
+++ components/menu/menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function Menu() {
+    return (
+        <div className='menu'>
+        
+            <ul>
+            <li>Home</li>
+            <li>Contacts</li>
+            <li>Portfolio</li>
+            <li>about</li>
+            </ul>
+        </div>
+    )
+}
---
+++ components/welcome/welcome.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import './welcome.css'
+import Image from 'next/image'
+export default function Welcome() {
+  return (
+    <div>
+    <div className="card">
+    <div className="dot-container-main">
+      <Image className="dot dot1	 size-5" width={30} height={30} src='/1 (1).jpg' alt=''/>
+      <Image className="dot dot2	 size-5"  width={30} height={30} src='/1 (2).jpg' alt=''/>
+      <Image className="dot dot3	 size-5"  width={30} height={30} src='/1 (3).jpg' alt=''/>
+      <Image className="dot dot4	 size-5"  width={30} height={30} src='/1 (4).jpg' alt=''/>
+      <Image className="dot dot5	 size-5"  width={30} height={30} src='/1 (5).jpg' alt=''/>
+      <Image className="dot dot6	 size-5"  width={30} height={30} src='/1 (6).jpg' alt=''/>
+      <Image className="dot dot7	 size-5"  width={30} height={30} src='/1 (7).jpg' alt=''/>
+      <Image className="dot dot8	 size-5"  width={30} height={30} src='/1 (8).jpg' alt=''/>
+      <Image className="dot dot9	 size-5"  width={30} height={30} src='/1 (15).jpg' alt=''/>
+      <Image className="dot dot10 size-5"  width={30} height={30} src='/1 (10).jpg' alt=''/>
+      <Image className="dot dot11 size-5"  width={30} height={30} src='/1 (11).jpg' alt=''/>
+      <Image className="dot dot12 size-5"  width={30} height={30} src='/1 (12).jpg' alt=''/>
+      <Image className="dot dot13 size-5"  width={30} height={30} src='/1 (13).jpg' alt=''/>
+      <Image className="dot dot14 size-5"  width={30} height={30} src='/1 (14).jpg' alt=''/>
+      <Image className="dot dot15 size-5"  width={30} height={30} src='/1 (15).jpg' alt=''/>
+      <Image className="dot dot16 size-5"  width={30} height={30} src='/1 (16).jpg' alt=''/>
+      <Image className="dot dot17 size-5"  width={30} height={30} src='/1 (17).jpg' alt=''/>
+      <Image className="dot dot18 size-5"  width={30} height={30} src='/1 (18).jpg' alt=''/>
+      <Image className="dot dot19 size-5"  width={30} height={30} src='/1 (19).jpg' alt=''/>
+      <Image className="dot dot20 size-5"  width={30} height={30} src='/1 (20).jpg' alt=''/>
+      <Image className="dot dot21 size-5"  width={30} height={30} src='/1 (21).jpg' alt=''/>
+      <Image className="dot dot22 size-5"  width={30} height={30} src='/1 (22).jpg' alt=''/>
+      <Image className="dot dot23 size-5"  width={30} height={30} src='/1 (23).jpg' alt=''/>
+      <Image className="dot dot24 size-5"  width={30} height={30} src='/1 (24).jpg' alt=''/>
+      <Image className="dot dot25 size-5"  width={30} height={30} src='/1 (1).jpg' alt=''/>
+      <Image className="dot dot26 size-5"  width={30} height={30} src='/1 (2).jpg' alt=''/>
+      <Image className="dot dot27 size-5"  width={30} height={30} src='/1 (3).jpg' alt=''/>
+      <Image className="dot dot28 size-5"  width={30} height={30} src='/1 (4).jpg' alt=''/>
+      <Image className="dot dot29 size-5"  width={30} height={30} src='/1 (5).jpg' alt=''/>
+      <Image className="dot dot30 size-5"  width={30} height={30} src='/1 (9).jpg' alt=''/>
+      <Image className="dot dot31 size-5"  width={30} height={30} src='/1 (7).jpg' alt=''/>
+      <Image className="dot dot32 size-5"  width={30} height={30} src='/1 (8).jpg' alt=''/>
+      <Image className="dot dot33 size-5"  width={30} height={30} src='/1 (9).jpg' alt=''/>
+      <Image className="dot dot34 size-5"  width={30} height={30} src='/1 (10).jpg' alt=''/>
+      <Image className="dot dot35 size-5"  width={30} height={30} src='/1 (11).jpg' alt=''/>
+      <Image className="dot dot36 size-5"  width={30} height={30} src='/1 (12).jpg' alt=''/>
+      <Image className="dot dot37 size-5"  width={30} height={30} src='/1 (13).jpg' alt=''/>
+      <Image className="dot dot38 size-5"  width={30} height={30} src='/1 (14).jpg' alt=''/>
+      <Image className="dot dot39 size-5"  width={30} height={30} src='/1 (24).jpg' alt=''/>
+      <Image className="dot dot40 size-5"  width={30} height={30} src='/1 (16).jpg' alt=''/>
+      <Image className="dot dot41 size-5"  width={30} height={30} src='/1 (17).jpg' alt=''/>
+      <Image className="dot dot42 size-5"  width={30} height={30} src='/1 (18).jpg' alt=''/>
+      <Image className="dot dot43 size-5"  width={30} height={30} src='/1 (19).jpg' alt=''/>
+      <Image className="dot dot44 size-5"  width={30} height={30} src='/1 (20).jpg' alt=''/>
+      <Image className="dot dot45 size-5"  width={30} height={30} src='/1 (21).jpg' alt=''/>
+      <Image className="dot dot46 size-5"  width={30} height={30} src='/1 (22).jpg' alt=''/>
+      <Image className="dot dot47 size-5"  width={30} height={30} src='/1 (23).jpg' alt=''/>
+      <Image className="dot dot48 size-5"  width={30} height={30} src='/1 (24).jpg' alt=''/>
+      <Image className="dot dot49 size-5"  width={30} height={30} src='/1 (1).jpg' alt=''/>
+      <Image className="dot dot50 size-5"  width={30} height={30} src='/1 (2).jpg' alt=''/>
+      <Image className="dot dot51 size-5"  width={30} height={30} src='/1 (3).jpg' alt=''/>
+      <Image className="dot dot52 size-10"  width={40} height={40} src='/1 (4).jpg' alt=''/>
+      <Image className="dot dot53 size-10"  width={40} height={40} src='/1 (5).jpg' alt=''/>
+      <Image className="dot dot54 size-10"  width={40} height={40} src='/1 (6).jpg' alt=''/>
+      <Image className="dot dot55 size-10"  width={40} height={40} src='/1 (7).jpg' alt=''/>
+      <Image className="dot dot56 size-10"  width={40} height={40} src='/1 (8).jpg' alt=''/>
+      <Image className="dot dot57 size-10"  width={40} height={40} src='/1 (9).jpg' alt=''/>
+      <Image className="dot dot58 size-10"  width={40} height={40} src='/1 (10).jpg' alt=''/>
+      <Image className="dot dot59 size-10"  width={40} height={40} src='/1 (11).jpg' alt=''/>
+      <Image className="dot dot60 size-10"  width={40} height={40} src='/1 (12).jpg' alt=''/>
+      <Image className="dot dot61 size-10"  width={40} height={40} src='/1 (13).jpg' alt=''/>
+      <Image className="dot dot62 size-10"  width={40} height={40} src='/1 (14).jpg' alt=''/>
+      <Image className="dot dot63 size-10"  width={40} height={40} src='/1 (15).jpg' alt=''/>
+      <Image className="dot dot64 size-10"  width={40} height={40} src='/1 (16).jpg' alt=''/>
+      <Image className="dot dot65 size-10"  width={40} height={40} src='/1 (17).jpg' alt=''/>
+      <Image className="dot dot66 size-10"  width={40} height={40} src='/1 (18).jpg' alt=''/>
+      <Image className="dot dot67 size-10"  width={40} height={40} src='/1 (19).jpg' alt=''/>
+      <Image className="dot dot68 size-10"  width={40} height={40} src='/1 (20).jpg' alt=''/>
+      <Image className="dot dot69 size-10"  width={40} height={40} src='/1 (21).jpg' alt=''/>
+      <Image className="dot dot70 size-10"  width={40} height={40} src='/1 (22).jpg' alt=''/>
+      <Image className="dot dot71 size-10"  width={40} height={40} src='/1 (23).jpg' alt=''/>
+      <Image className="dot dot72 size-15"  width={50} height={50} src='/1 (24).jpg' alt=''/>
+      <Image className="dot dot73 size-15"  width={50} height={50} src='/1 (1).jpg' alt=''/>
+      <Image className="dot dot74 size-15"  width={50} height={50} src='/1 (2).jpg' alt=''/>
+      <Image className="dot dot75 size-15"  width={50} height={50} src='/1 (3).jpg' alt=''/>
+      <Image className="dot dot76 size-20"  width={60} height={60} src='/1 (4).jpg' alt=''/>
+      <Image className="dot dot77 size-20"  width={60} height={60} src='/1 (5).jpg' alt=''/>
+      <Image className="dot dot78 size-20"  width={60} height={60} src='/1 (6).jpg' alt=''/>
+      <Image className="dot dot79 size-15"  width={50} height={50} src='/1 (7).jpg' alt=''/>
+      <Image className="dot dot80	size-5"  width={30} height={30} src='/1 (8).jpg' alt=''/>
+      <Image className="dot dot81	size-5"  width={30} height={30} src='/1 (9).jpg' alt=''/>
+      <Image className="dot dot82	size-5"  width={30} height={30} src='/1 (10).jpg' alt=''/>
+      <Image className="dot dot83	size-5"  width={30} height={30} src='/1 (11).jpg' alt=''/>
+    </div>
+  </div>
+    </div>
+  )
+}
